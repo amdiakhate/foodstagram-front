386e91c7141ecd6456fbeecda8055dad312184c5
Administrator <admin@example.com>
2015-09-30 00:27:41 +0200

    Upvote

--- js/components/feed/controllers/feedController.js
+++ js/components/feed/controllers/feedController.js
@@ -2,10 +2,13 @@
  * Created by Makhtar on 29/09/2015.
  */
 
-angular.module('foodstagramApp').controller('feedCtrl',['$scope','Photo', function ($scope,Photo) {
+angular.module('foodstagramApp').controller('feedCtrl', ['$scope', 'Photo', '$http', function ($scope, Photo, $http) {
+    var url = "http://localhost/foodstagram/web/app_dev.php/api"
     $scope.loading = true;
     Photo.query(function (data) {
         $scope.photos = data;
         $scope.loading = false;
     })
+
+
 }]);
--- js/components/feed/directives/photo_block.js
+++ js/components/feed/directives/photo_block.js
@@ -4,9 +4,22 @@
 
 
 angular.module('foodstagramApp')
-    .directive('photoBlock',function(){
+    .directive('photoBlock', function ($http) {
         return {
-            restrict : 'E',
-            templateUrl : 'js/components/feed/views/photo_block.html'
+            restrict: 'E',
+            templateUrl: 'js/components/feed/views/photo_block.html',
+            link: function (scope, element, attrs) {
+                element.find('.icon-arrow-up').bind('click', function () {
+                    var url = "http://localhost/foodstagram/web/app_dev.php/api"
+                    $http.post(url + '/photo/upvote', {
+                        'id_photo': scope.photo.id_photo
+                    }).then(
+                        function (response) {
+                            scope.photo = response.data;
+                        }
+                    )
+
+                })
+            }
         }
     });
