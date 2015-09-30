f671b261a37a833fa760686ff75b99581352bbdd
amdiakhate <amdiakhate@gismartware.com>
2015-09-30 14:06:21 +0200

    downvoting

--- js/components/feed/controllers/feedController.js
+++ js/components/feed/controllers/feedController.js
@@ -3,7 +3,6 @@
  */
 
 angular.module('foodstagramApp').controller('feedCtrl', ['$scope', 'Photo', '$http', function ($scope, Photo, $http) {
-    var url = "http://localhost/foodstagram/web/app_dev.php/api"
     $scope.loading = true;
     Photo.query(function (data) {
         $scope.photos = data;
--- js/components/feed/directives/photo_block.js
+++ js/components/feed/directives/photo_block.js
@@ -9,9 +9,22 @@ angular.module('foodstagramApp')
             restrict: 'E',
             templateUrl: 'js/components/feed/views/photo_block.html',
             link: function (scope, element, attrs) {
+                //Upvoting
                 element.find('.icon-arrow-up').bind('click', function () {
-                    var url = "http://localhost/foodstagram/web/app_dev.php/api"
+                    var url = "http://localhost:8082/foodstagram/web/app_dev.php/api"
                     $http.post(url + '/photo/upvote', {
+                        'id_photo': scope.photo.id_photo
+                    }).then(
+                        function (response) {
+                            scope.photo = response.data;
+                        }
+                    )
+
+                })
+                //Downvoting
+                element.find('.icon-arrow-down').bind('click', function () {
+                    var url = "http://localhost:8082/foodstagram/web/app_dev.php/api"
+                    $http.post(url + '/photo/downvote', {
                         'id_photo': scope.photo.id_photo
                     }).then(
                         function (response) {
