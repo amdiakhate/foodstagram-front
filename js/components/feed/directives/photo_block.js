76335e6645a285005d03f582a94fe2b2d57fa1c8
amdiakhate <amdiakhate@gismartware.com>
2015-09-30 17:35:12 +0200

    Fix for the $httpprovider

--- js/components/feed/directives/photo_block.js
+++ js/components/feed/directives/photo_block.js
@@ -11,7 +11,7 @@ angular.module('foodstagramApp')
             link: function (scope, element, attrs) {
                 //Upvoting
                 element.find('.icon-arrow-up').bind('click', function () {
-                    var url = "http://localhost:8082/foodstagram/web/app_dev.php/api"
+                    var url = "http://foodstagram.lifeswift.fr/api"
                     $http.post(url + '/photo/upvote', {
                         'id_photo': scope.photo.id_photo
                     }).then(
@@ -23,7 +23,7 @@ angular.module('foodstagramApp')
                 })
                 //Downvoting
                 element.find('.icon-arrow-down').bind('click', function () {
-                    var url = "http://localhost:8082/foodstagram/web/app_dev.php/api"
+                    var url = "http://foodstagram.lifeswift.fr/api"
                     $http.post(url + '/photo/downvote', {
                         'id_photo': scope.photo.id_photo
                     }).then(
