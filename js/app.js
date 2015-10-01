340096c8c2db440ca11631fbc2cc6ce0a7a9edf0
Administrator <admin@example.com>
2015-10-01 22:20:56 +0200

    LocalStorage doublons

--- js/app.js
+++ js/app.js
@@ -2,21 +2,21 @@
  * Created by Makhtar on 19/09/2015.
  */
 
-angular.module('foodstagramApp', ['ngResource'], function($httpProvider) {
+angular.module('foodstagramApp', ['ngResource', 'LocalStorageModule'], function ($httpProvider) {
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
     /**
      * The workhorse; converts an object to x-www-form-urlencoded serialization.
      * @param {Object} obj
      * @return {String}
      */
-    var param = function(obj) {
+    var param = function (obj) {
         var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
 
-        for(name in obj) {
+        for (name in obj) {
             value = obj[name];
 
-            if(value instanceof Array) {
-                for(i=0; i<value.length; ++i) {
+            if (value instanceof Array) {
+                for (i = 0; i < value.length; ++i) {
                     subValue = value[i];
                     fullSubName = name + '[' + i + ']';
                     innerObj = {};
@@ -24,8 +24,8 @@ angular.module('foodstagramApp', ['ngResource'], function($httpProvider) {
                     query += param(innerObj) + '&';
                 }
             }
-            else if(value instanceof Object) {
-                for(subName in value) {
+            else if (value instanceof Object) {
+                for (subName in value) {
                     subValue = value[subName];
                     fullSubName = name + '[' + subName + ']';
                     innerObj = {};
@@ -33,7 +33,7 @@ angular.module('foodstagramApp', ['ngResource'], function($httpProvider) {
                     query += param(innerObj) + '&';
                 }
             }
-            else if(value !== undefined && value !== null)
+            else if (value !== undefined && value !== null)
                 query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
         }
 
@@ -41,72 +41,14 @@ angular.module('foodstagramApp', ['ngResource'], function($httpProvider) {
     };
 
     // Override $http service's default transformRequest
-    $httpProvider.defaults.transformRequest = [function(data) {
+    $httpProvider.defaults.transformRequest = [function (data) {
         return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
     }];
+}).config(function (localStorageServiceProvider) {
+    localStorageServiceProvider
+        .setPrefix('foodstagram')
+        .setStorageType('localStorage');
+    ;
 });
+;
 
-
-//
-//foodstagramApp.controller('GreetingsCtrl', ['$scope', function ($scope) {
-//    $scope.greeting = 'Bonjour ';
-//    $scope.message = "hello"
-//}]);
-//
-//foodstagramApp.controller('DoubleCtrl', ['$scope', function ($scope) {
-//    $scope.double = 4;
-//}]);
-//
-//foodstagramApp.controller('TripleCtrl', ['$scope', function ($scope) {
-//    console.log($scope.double);
-//
-//    $scope.triple = function (value) {
-//        return $scope.double * value * 3;
-//    }
-//}])
-//
-//foodstagramApp.directive('welcome', function () {
-//    return {
-//        restrict: 'E',
-//        controller: function ($scope) {
-//            $scope.words = [];
-//            this.sayHello = function () {
-//                $scope.words.push('hello');
-//            }
-//
-//            this.sayHi = function () {
-//                $scope.words.push('hi');
-//            }
-//        },
-//        link: function (scope, element) {
-//            element.bind('mouseenter', function () {
-//                console.log(scope.words);
-//            })
-//        }
-//    }
-//})
-//
-//foodstagramApp.directive('hello', function () {
-//    return {
-//        require : 'welcome',
-//        link: function (scope, element, attrs, welcomeCtrl) {
-//            element.bind('mouseenter', function () {
-//                welcomeCtrl.sayHello();
-//            })
-//        },
-//        template : '<p>hello</p>'
-//    }
-//})
-//
-//foodstagramApp.directive('hi', function () {
-//    return {
-//        require : 'welcome',
-//        link: function (scope, element, attrs, welcomeCtrl) {
-//            element.bind('mouseenter', function () {
-//                welcomeCtrl.sayHi();
-//            })
-//        },
-//        template : '<p>hi</p>'
-//
-//    }
-//})
--- js/components/feed/controllers/feedController.js
+++ js/components/feed/controllers/feedController.js
@@ -2,13 +2,18 @@
  * Created by Makhtar on 29/09/2015.
  */
 
-angular.module('foodstagramApp').controller('feedCtrl', ['$scope', 'Photos', '$http', function ($scope, Photos, $http) {
+angular.module('foodstagramApp').controller('feedCtrl', ['$scope', 'Photos', 'localStorageService', function ($scope, Photos, localStorageService) {
+
     $scope.loading = true;
     $scope.searchText = '';
     $scope.page = 1;
     //No photos at the beginning
     $scope.photos = [];
     $scope.photo = null;
+
+    if (!localStorageService.get('photos')){
+        localStorageService.set('photos', []);
+    }
 
     //This function loads the photos
     $scope.loadMore = function () {
@@ -20,7 +25,13 @@ angular.module('foodstagramApp').controller('feedCtrl', ['$scope', 'Photos', '$h
                 $scope.loading = false;
                 $scope.page++;
 
-                $scope.photo = $scope.photos.shift();
+                var photo = $scope.photos.shift();
+                //Everytime we show a new photo, we add it to the localStorage
+                if (checkPhoto(photo)) {
+                    $scope.photo = photo;
+                } else {
+                    $scope.nextPhoto();
+                }
             } else {
                 $('.load-more').text('No more data');
             }
@@ -32,9 +43,14 @@ angular.module('foodstagramApp').controller('feedCtrl', ['$scope', 'Photos', '$h
     /**
      * This function loads the next photo
      */
-    $scope.nextPhoto = function() {
+    $scope.nextPhoto = function () {
         if ($scope.photos.length > 0) {
-            $scope.photo = $scope.photos.shift();
+            var photo = $scope.photos.shift();
+            if (checkPhoto(photo)) {
+                $scope.photo = photo;
+            } else {
+                $scope.nextPhoto();
+            }
         } else {
             $scope.loadMore();
         }
@@ -43,6 +59,28 @@ angular.module('foodstagramApp').controller('feedCtrl', ['$scope', 'Photos', '$h
     //Search by clicking on tags
     $scope.updateSearch = function (tag) {
         $scope.searchText = tag;
+    }
+
+    function checkPhoto(photo) {
+        var storage = localStorageService.get('photos');
+        var id_photo = photo.id_photo;
+        if (storage.length == 0) {
+            var storedPhotos = [id_photo];
+            localStorageService.set('photos', JSON.stringify(storedPhotos));
+        } else {
+            var storedPhotos = JSON.parse(storage);
+            if (storedPhotos.indexOf(id_photo) == -1) {
+                //We don't find the photo, we show it
+                storedPhotos.push(id_photo);
+                localStorageService.set('photos', JSON.stringify(storedPhotos));
+                return true;
+            } else {
+                //We find it, we should pass to the next photo
+                return false;
+            }
+        }
+        return true;
+
     }
 
 }]);
